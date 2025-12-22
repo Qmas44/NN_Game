c0b542b99d1c190d13bb72d87cac3e1d04968a66
Quentin Terry <Qterry44@gmail.com>
2025-12-22 14:42:34 -0500

    Create Simple Inventory Display script

---
+++ Assets/ThirdParty/TopDownEngine/ThirdParty/MoreMountains/InventoryEngine/InventoryEngine/Scripts/GUI/SimpleInventoryDisplay.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using MoreMountains.Tools;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.EventSystems;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace MoreMountains.InventoryEngine
+{	
+	[SelectionBase]
+	/// <summary>
+	/// A component that handles the visual representation of an Inventory, allowing the user to interact with it
+	/// </summary>
+	public class SimpleInventoryDisplay : MonoBehaviour
+	{
+		[Header("Binding")]
+		/// the name of the inventory to display
+		/// <summary>
+		/// Grabs the target inventory based on its name
+		/// </summary>
+		/// <value>The target inventory.</value>
+		public Inventory TargetInventory;
+
+		public int AmmoCount;
+		public string AmmoItemID = "BaseCharacterTestGunAmmo";
+
+		void Update()
+		{
+			AmmoCount = TargetInventory.GetQuantity(AmmoItemID);
+		}
+		
+	}
+}
